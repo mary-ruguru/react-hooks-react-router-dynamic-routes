dce70991efea0cbc94dad2c0f64ec725646e5f88
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-01-17 00:52:10 +0300

    understood

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+// import the custom `useRouteMatch` hook from React Router
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+// import the MovieShow component
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  // useRouteMatch returns a special object with information about
+  // the currently matched route
+  const match = useRouteMatch();
+  console.log(match);
+
   return (
     <div>
       <MoviesList movies={movies} />
+      {
+         <Route exact path={match.url}>
+         <h3>Choose a movie from the list above</h3>
+       </Route>
+ 
+      }
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies = {movies}/>
+      </Route>
     </div>
   );
 }
